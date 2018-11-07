bc818ed1165abfdde100ff6c2a7a70bd15afaf17
Sharath George M <sharathgeorgem@gmail.com>
2018-11-07 06:21:27 +0530

    Updates

---
+++ precache-manifest.15f06672c8fec657fc5efc8e0e26e46a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "d8b7840e1599c43493c5",
+    "url": "/hackernews/static/js/main.d8b7840e.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/hackernews/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "d8b7840e1599c43493c5",
+    "url": "/hackernews/static/css/main.3430dd06.chunk.css"
+  },
+  {
+    "revision": "070776aadb7567626692e80a2da97587",
+    "url": "/hackernews/index.html"
+  }
+];
