f0ef22e4c36db0f29d3e2ee342a2374d6e344d64
Sharath George M <sharathgeorgem@gmail.com>
2018-11-07 20:39:49 +0530

    Component renders sample item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
 import './App.css'
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clarke',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
 class App extends Component {
-  render () {
-    const user = {
-      firstName: 'Sharath',
-      lastName: 'George M'
+  constructor (props) {
+    super(props)
+    this.state = {
+      list
     }
-    console.log(user.lastName)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+  onDismiss (id) {
+    const isNotId = item => item.objectID !== id
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({list: updatedList})
+  }
+  render () {
     return (
       <div className='App'>
-        <h1>{user.firstName}</h1>
-        <h2>{user.lastName}</h2>
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)} type='button'>
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     )
   }
