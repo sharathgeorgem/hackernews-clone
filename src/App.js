89e11c9c1ce65fc71b499742552d3331ce7ab11a
Sharath George M <sharathgeorgem@gmail.com>
2018-11-08 06:14:16 +0530

    Changed sample input

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css'
 
 const list = [
   {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
+    title: 'Steam',
+    url: 'https://store.steampowered.com/',
+    developer: 'Valve',
     num_comments: 3,
     points: 4,
     objectID: 0
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clarke',
+    title: 'Origin',
+    url: 'https://www.origin.com/ind/en-us/store',
+    developer: 'Effin EA',
     num_comments: 2,
     points: 5,
     objectID: 1
@@ -39,7 +39,7 @@ class App extends Component {
         {this.state.list.map(item =>
           <div key={item.objectID}>
             <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
+            <span>{item.developer}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
